0a81e7f9d76abd7d162592012c04c74785531016
adhywiranto44 <adhywiranto68@gmail.com>
2021-05-10 17:16:48 +0700

    Menyelesaikan routing di frontend

--- app.js
+++ app.js
@@ -1,22 +1,275 @@
+// Require Modules
+require('dotenv').config();
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const PORT = 3000;
 
+// Express
 const app = express();
 
+// Use
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Set Modules
 app.set("view engine", "ejs");
 
+// MongoDB
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    created_at: Date,
+    updated_at: Date
+})
+userSchema.plugin(passportLocalMongoose);
+
+const categorySchema = mongoose.Schema({
+    name: String,
+    slug: String
+})
+
+const hobbySchema = mongoose.Schema({
+    name: String,
+    slug: String,
+    description: String,
+    category: [categorySchema],
+    img: String,
+    visited_count: Number,
+    created_at: Date,
+    updated_at: Date
+})
+
+const suggestionSchema = mongoose.Schema({
+    name: String,
+    slug: String,
+    description: String,
+    category: categorySchema,
+    created_at: Date,
+    updated_at: Date
+})
+
+const User = mongoose.model("User", userSchema);
+const Category = mongoose.model("Category", categorySchema);
+const Hobby = mongoose.model("Hobby", hobbySchema);
+const Suggestion = mongoose.model("Suggestion", suggestionSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+const category1 = new Category({
+    name: "Teknologi",
+    slug: "teknologi"
+})
+
+const category2 = new Category({
+    name: "Audio Visual",
+    slug: "audio-visual"
+})
+
+const category3 = new Category({
+    name: "Sastra",
+    slug: "sastra"
+})
+
+const category4 = new Category({
+    name: "Kerajinan",
+    slug: "kerajinan"
+})
+
+const category5 = new Category({
+    name: "Otomotif",
+    slug: "otomotif"
+})
+
+const menyanyi = new Hobby({
+    name: "Menyanyi",
+    slug: "menyanyi",
+    description: "Menyanyi merupakan salah satu hobi yang populer di dunia.",
+    category: [{
+        _id: "6098e75d306eb4115cbe2619",
+        name: "Audio Visual",
+        slug: "audio-visual"
+    }],
+    img: "",
+    visited_count: 0,
+    created_at: Date(),
+    updated_at: Date()
+})
+
+const menari = new Hobby({
+    name: "Menari",
+    slug: "menari",
+    description: "Menari merupakan salah satu hobi yang populer di dunia.",
+    category: [{
+        _id: "6098e75d306eb4115cbe2619",
+        name: "Audio Visual",
+        slug: "audio-visual"
+    }],
+    img: "",
+    visited_count: 0,
+    created_at: Date(),
+    updated_at: Date()
+})
+
+const coding = new Hobby({
+    name: "Coding",
+    slug: "coding",
+    description: "Coding adalah memuat program seperti website.",
+    category: [{
+        _id: "6098e75d306eb4115cbe2618",
+        name: "Teknologi",
+        slug: "teknologi"
+    }],
+    img: "",
+    visited_count: 0,
+    created_at: Date(),
+    updated_at: Date()
+})
+
+// User.register({username: "admin", created_at: Date(), updated_at: Date()}, "1234");
+// Category.insertMany([category1, category2, category3, category4, category5]);
+// menyanyi.save();
+// Hobby.insertMany([menari, coding]);
+
+
+// Routes
 app.get("/", (req, res) => {
-    res.render("index", {currentDate: new Date().getFullYear()});
+    Hobby.find((err, foundHobbies) => {
+        if (err) {
+            res.redirect("/");
+        } else {
+            if (foundHobbies !== null) {
+                Category.find((err, foundCategory) => {
+                    res.render("index", {currentDate: new Date().getFullYear(), hobbies: foundHobbies, categories: foundCategory});
+                })
+            } else {
+                res.redirect("/");
+            }
+        }
+    }).limit(5).sort({visited_count: -1});
 })
 
-app.get("/kategori/hobi", (req, res) => {
-    res.render("hobi", {currentDate: new Date().getFullYear()});
+app.post("/", (req, res) => {
+    res.redirect("/s/"+req.body.search);
 })
 
-app.get("/s/hobi", (req, res) => {
-    res.render("cari-hobi");
+app.get("/a/:kategori", (req, res) => {
+    const kategori = req.params.kategori;
+
+    Category.findOne({slug: kategori}, (err, foundCategory) => {
+        if (err) {
+            res.redirect("/");
+        } else {
+            if (foundCategory !== null) {
+                Hobby.find({"category._id": foundCategory._id}, (err, foundHobbies) => {
+                    if (err) {
+                        res.redirect("/");
+                    } else {
+                        if (foundHobbies !== null && foundHobbies.length > 0) {
+                            res.render("cari-hobi", {currentDate: new Date().getFullYear(), kind: "kategori", hobbies: foundHobbies});
+                        } else {
+                            res.redirect("/");
+                        }
+                    }
+                })
+            } else {
+                res.redirect("/");
+            }
+        }
+    })
+})
+
+app.get("/d/:kategori/:hobi", (req, res) => {
+    const kategori = req.params.kategori;
+    const hobi = req.params.hobi;
+
+    Category.findOne({slug: kategori}, (err, foundCategory) => {
+        if (err) {
+            res.redirect("/");
+        } else {
+            if (foundCategory !== null) {
+                Hobby.findOne({slug: hobi, "category._id": foundCategory._id}, (err, foundHobby) => {
+                    if (err) {
+                        res.redirect("/");
+                    } else {
+                        if (foundHobby !== null) {
+                            let visited_count = foundHobby.visited_count;
+                            visited_count++;
+                            Hobby.findOneAndUpdate({slug: foundHobby.slug}, {visited_count}, (err, hobbyChanged) => {
+                                if (err) {
+                                    res.redirect("/"); 
+                                } else {
+                                    res.render("hobi", {currentDate: new Date().getFullYear(), hobby: foundHobby});
+                                }
+                            })
+                        } else {
+                            res.redirect("/");
+                        }
+                    }
+                })
+            } else {
+                res.redirect("/");
+            }
+        }
+    })
+})
+
+app.get("/s/", (req, res) => {
+
+    Hobby.find((err, foundHobbies) => {
+        if (err) {
+            res.redirect("/");
+        } else {
+            if (foundHobbies !== null) {
+                res.render("cari-hobi", {currentDate: new Date().getFullYear(), kind: "", hobbies: foundHobbies});
+            } else {
+                res.redirect("/");
+            }
+        }
+    })
+})
+
+app.get("/s/:search", (req, res) => {
+    const search = req.params.search;
+
+    Hobby.find({name: {$regex: ".*"+search+".*", $options: 'i'}}, (err, foundHobbies) => {
+        if (err) {
+            res.redirect("/");
+        } else {
+            if (foundHobbies !== null) {
+                res.render("cari-hobi", {currentDate: new Date().getFullYear(), kind: "mencari", hobbies: foundHobbies, search});
+            } else {
+                res.redirect("/");
+            }
+        }
+    })
+})
+
+app.get("/carikan-saya-hobi", (req, res) => {
+    Hobby.findOne((err, foundHobby) => {
+        if (err) {
+            res.redirect("/");
+        } else {
+            const recommendedHobby = foundHobby.slug;
+            const hobbyCategory = foundHobby.category[0].slug;
+            res.redirect(`/d/${hobbyCategory}/${recommendedHobby}`);
+        }
+    }).sort({visited_count: -1});
 })
 
 app.get("/saran-hobi", (req, res) => {
