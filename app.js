2294d2f08e197c5d691fde9390c4ee4a8f496e51
adhywiranto44 <adhywiranto68@gmail.com>
2021-05-19 09:01:39 +0700

    Helper showAlert dan memindahkan data dummy ke folder helper

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {Category} = require("./models/Category.js");
 const Hobby = require("./models/Hobby.js");
 const Suggestion = require("./models/Suggestion.js");
 const User = require("./models/User.js");
+const showAlert = require("./helpers/showAlert.js");
 const PORT = 3000;
 
 // Express
@@ -38,134 +39,6 @@ mongoose.set("useCreateIndex", true);
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-const showAlert = function(color, message) {
-    return `<div class="alert ${color} alert-dismissible fade show" role="alert">${message}<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;
-}
-
-// const category1 = new Category({
-//     name: "Teknologi",
-//     slug: "teknologi"
-// })
-
-// const category2 = new Category({
-//     name: "Audio Visual",
-//     slug: "audio-visual"
-// })
-
-// const category3 = new Category({
-//     name: "Sastra",
-//     slug: "sastra"
-// })
-
-// const category4 = new Category({
-//     name: "Kerajinan",
-//     slug: "kerajinan"
-// })
-
-// const category5 = new Category({
-//     name: "Otomotif",
-//     slug: "otomotif"
-// })
-
-// const menyanyi = new Hobby({
-//     name: "Menyanyi",
-//     slug: "menyanyi",
-//     description: "Menyanyi merupakan salah satu hobi yang populer di dunia.",
-//     category: [{
-//         _id: "6098e75d306eb4115cbe2619",
-//         name: "Audio Visual",
-//         slug: "audio-visual"
-//     }],
-//     img: "",
-//     visited_count: 0,
-//     created_at: Date(),
-//     updated_at: Date()
-// })
-
-// const menari = new Hobby({
-//     name: "Menari",
-//     slug: "menari",
-//     description: "Menari merupakan salah satu hobi yang populer di dunia.",
-//     category: [{
-//         _id: "6098e75d306eb4115cbe2619",
-//         name: "Audio Visual",
-//         slug: "audio-visual"
-//     }],
-//     img: "",
-//     visited_count: 0,
-//     created_at: Date(),
-//     updated_at: Date()
-// })
-
-// const coding = new Hobby({
-//     name: "Coding",
-//     slug: "coding",
-//     description: "Coding adalah memuat program seperti website.",
-//     category: [{
-//         _id: "6098e75d306eb4115cbe2618",
-//         name: "Teknologi",
-//         slug: "teknologi"
-//     }],
-//     img: "",
-//     visited_count: 0,
-//     created_at: Date(),
-//     updated_at: Date()
-// })
-
-// const mendayung = new Suggestion({
-//     name: "Mendayung perahu",
-//     slug: "mendayung-perahu",
-//     description: "ya mendayung",
-//     category: [{
-//         _id: "6098e75d306eb4115cbe2618",
-//         name: "Teknologi",
-//         slug: "teknologi"
-//     }],
-//     img: "",
-//     visited_count: 0,
-//     suggester_email: "adhy@gmail.com",
-//     created_at: Date(),
-//     updated_at: Date()
-// })
-
-// const memancing = new Suggestion({
-//     name: "Memancing",
-//     slug: "memancing",
-//     description: "ya mendayung",
-//     category: [{
-//         _id: "6098e75d306eb4115cbe2618",
-//         name: "Teknologi",
-//         slug: "teknologi"
-//     }],
-//     img: "",
-//     visited_count: 0,
-//     suggester_email: "adhy@gmail.com",
-//     created_at: Date(),
-//     updated_at: Date()
-// })
-
-// const berlayar = new Suggestion({
-//     name: "Berlayar",
-//     slug: "berlayar",
-//     description: "ya mendayung",
-//     category: [{
-//         _id: "6098e75d306eb4115cbe2618",
-//         name: "Teknologi",
-//         slug: "teknologi"
-//     }],
-//     img: "",
-//     visited_count: 0,
-//     suggester_email: "adhy@gmail.com",
-//     created_at: Date(),
-//     updated_at: Date()
-// })
-
-// User.register({username: "admin", created_at: Date(), updated_at: Date()}, "1234");
-// Category.insertMany([category1, category2, category3, category4, category5]);
-// menyanyi.save();
-// Hobby.insertMany([menari, coding]);
-// Suggestion.insertMany([mendayung, memancing, berlayar]);
 
 // Routes
 app.get("/", (req, res) => {
---
+++ helpers/dummy_data.js
@@ -0,0 +1,117 @@
+// const category1 = new Category({
+//     name: "Teknologi",
+//     slug: "teknologi"
+// })
+
+// const category2 = new Category({
+//     name: "Audio Visual",
+//     slug: "audio-visual"
+// })
+
+// const category3 = new Category({
+//     name: "Sastra",
+//     slug: "sastra"
+// })
+
+// const category4 = new Category({
+//     name: "Kerajinan",
+//     slug: "kerajinan"
+// })
+
+// const category5 = new Category({
+//     name: "Otomotif",
+//     slug: "otomotif"
+// })
+
+// const menyanyi = new Hobby({
+//     name: "Menyanyi",
+//     slug: "menyanyi",
+//     description: "Menyanyi merupakan salah satu hobi yang populer di dunia.",
+//     category: [{
+//         _id: "6098e75d306eb4115cbe2619",
+//         name: "Audio Visual",
+//         slug: "audio-visual"
+//     }],
+//     img: "",
+//     visited_count: 0,
+//     created_at: Date(),
+//     updated_at: Date()
+// })
+
+// const menari = new Hobby({
+//     name: "Menari",
+//     slug: "menari",
+//     description: "Menari merupakan salah satu hobi yang populer di dunia.",
+//     category: [{
+//         _id: "6098e75d306eb4115cbe2619",
+//         name: "Audio Visual",
+//         slug: "audio-visual"
+//     }],
+//     img: "",
+//     visited_count: 0,
+//     created_at: Date(),
+//     updated_at: Date()
+// })
+
+// const coding = new Hobby({
+//     name: "Coding",
+//     slug: "coding",
+//     description: "Coding adalah memuat program seperti website.",
+//     category: [{
+//         _id: "6098e75d306eb4115cbe2618",
+//         name: "Teknologi",
+//         slug: "teknologi"
+//     }],
+//     img: "",
+//     visited_count: 0,
+//     created_at: Date(),
+//     updated_at: Date()
+// })
+
+// const mendayung = new Suggestion({
+//     name: "Mendayung perahu",
+//     slug: "mendayung-perahu",
+//     description: "ya mendayung",
+//     category: [{
+//         _id: "6098e75d306eb4115cbe2618",
+//         name: "Teknologi",
+//         slug: "teknologi"
+//     }],
+//     img: "",
+//     visited_count: 0,
+//     suggester_email: "adhy@gmail.com",
+//     created_at: Date(),
+//     updated_at: Date()
+// })
+
+// const memancing = new Suggestion({
+//     name: "Memancing",
+//     slug: "memancing",
+//     description: "ya mendayung",
+//     category: [{
+//         _id: "6098e75d306eb4115cbe2618",
+//         name: "Teknologi",
+//         slug: "teknologi"
+//     }],
+//     img: "",
+//     visited_count: 0,
+//     suggester_email: "adhy@gmail.com",
+//     created_at: Date(),
+//     updated_at: Date()
+// })
+
+// const berlayar = new Suggestion({
+//     name: "Berlayar",
+//     slug: "berlayar",
+//     description: "ya mendayung",
+//     category: [{
+//         _id: "6098e75d306eb4115cbe2618",
+//         name: "Teknologi",
+//         slug: "teknologi"
+//     }],
+//     img: "",
+//     visited_count: 0,
+//     suggester_email: "adhy@gmail.com",
+//     created_at: Date(),
+//     updated_at: Date()
+// })
