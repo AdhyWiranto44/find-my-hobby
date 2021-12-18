16237ab0d8a3edd6694e26a883bbb7a2babe9a62
Adhy Wiranto <adhywiranto68@gmail.com>
2021-12-18 14:25:15 +0700

    refactor: dashboard controller

--- src/controllers/dashboard_controller.js
+++ src/controllers/dashboard_controller.js
@@ -2,28 +2,34 @@ const Hobby = require("../models/hobby");
 const Suggestion = require("../models/suggestion");
 
 
-exports.index = (req, res) => {
-    if (req.isAuthenticated()) {
-        let hobbies = null
-        let suggestions = null
-
-        Hobby.find().exec()
-
-        .then(foundHobbies => {
-            hobbies = foundHobbies
-            return Suggestion.find().exec()
-        })
-
-        .then(foundSuggestions => {
-            suggestions = foundSuggestions
-            res.render("dashboard", {title: "Dashboard", hobbies_length: hobbies.length, suggestions_length: suggestions.length})
-        })
-
-        .catch(err => {
-            console.error(err)
-        })
-        
-    } else {
-        res.redirect("/auth/login")
+exports.index = async (req, res) => {
+    if (!req.isAuthenticated()) {
+        return res.redirect("/auth/login");
     }
+    
+    let hobbies = [];
+    let suggestions = [];
+
+    await Hobby.find().exec()
+    .then(foundHobbies => {
+        hobbies = [...foundHobbies];
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    
+    await Suggestion.find().exec()
+    .then(foundSuggestions => {
+        suggestions = [...foundSuggestions];
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+    const data = {
+        title: "Dashboard", 
+        hobbies_length: hobbies.length, 
+        suggestions_length: suggestions.length
+    }
+    res.render("dashboard", data);
 }
