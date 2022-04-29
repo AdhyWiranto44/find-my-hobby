56805d4b3cdbe7989d9a2c75a59920cfb1ed4b75
Adhy Wiranto <adhywiranto68@gmail.com>
2022-04-29 19:52:38 +0700

    feat: createConnection

--- backend/src/database/Connection.ts
+++ backend/src/database/Connection.ts
@@ -1,16 +1,30 @@
-import { connect } from "mongoose";
+import { connect, createConnection } from "mongoose";
+import { LOCAL_ENV, PRODUCTION_ENV } from "../helpers/constants";
 
 
 class Connection {
+  localConnectionURI: string = `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`;
+  cloudConnectionURI: string = `mongodb+srv://find-my-hobby-admin:${process.env.DB_PASSWORD}@cluster0.k9fdy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
   connect() {
     switch(process.env.ENVIRONMENT) {
-      case "local":
-        return connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`);
-      case "production":
-        return connect(`mongodb+srv://find-my-hobby-admin:${process.env.DB_PASSWORD}@cluster0.k9fdy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`);
+      case LOCAL_ENV:
+        return connect(this.localConnectionURI);
+      case PRODUCTION_ENV:
+        return connect(this.cloudConnectionURI);
       default:
-        return connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`);
+        return connect(this.localConnectionURI);
+    }
+  }
+
+  createConnection() {
+    switch(process.env.ENVIRONMENT) {
+      case LOCAL_ENV:
+        return createConnection(this.localConnectionURI);
+      case PRODUCTION_ENV:
+        return createConnection(this.cloudConnectionURI);
+      default:
+        return createConnection(this.localConnectionURI);
     }
   }
 }
