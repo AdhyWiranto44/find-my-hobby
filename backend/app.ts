364ca50ff3d2a50182365a428c7b20e3987a114e
Adhy Wiranto <adhywiranto68@gmail.com>
2022-04-30 15:25:42 +0700

    feat: cors option

--- backend/app.ts
+++ backend/app.ts
@@ -4,12 +4,15 @@ import Connection from "./src/database/Connection";
 import v1 from './src/routers/api';
 import cors from 'cors';
 
-
+const corsOptions = {
+  origin: process.env.ORIGIN,
+  optionsSuccessStatus: 200 // For legacy browser support
+}
 const PORT = 8080;
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: true}));
 app.use("/api/v1/", v1);
 app.set("view engine", "ejs");
