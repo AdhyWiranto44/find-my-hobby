dd108a685871e2bbb4cf65eec8888fc51ba82569
Adhy Wiranto <adhywiranto68@gmail.com>
2022-04-30 08:25:47 +0700

    feat: generate default hobby using faker data

--- backend/src/helpers/dummy_data.ts
+++ backend/src/helpers/dummy_data.ts
@@ -1,4 +1,5 @@
 import { hashSync } from "bcrypt";
+import { faker } from '@faker-js/faker';
 
 
 export const default_categories = [
@@ -24,32 +25,46 @@ export const default_categories = [
   }
 ];
 
-export const default_hobbies = [
-  {
-    name: "Menyanyi",
-    slug: "menyanyi",
-    description: "Menyanyi merupakan salah satu hobi yang populer di dunia.",
-    category: "audio-visual",
-    img: "",
-    visited_count: 0
-  },
-  {
-    name: "Menari",
-    slug: "menari",
-    description: "Menari merupakan salah satu hobi yang populer di dunia.",
-    category: "audio-visual",
-    img: "",
-    visited_count: 0
-  },
-  {
-    name: "Coding",
-    slug: "coding",
-    description: "Coding adalah memuat program seperti website.",
-    category: "teknologi",
+export const default_hobbies: any[] = [];
+for (let i = 0; i < 50; i++) {
+  const categories = ["teknologi", "audio-visual", "sastra", "kerajinan", "otomotif"];
+  const hobby: any = {
+    name: faker.name.jobTitle(),
+    slug: faker.name.jobTitle().replace(/\s+/g, '-').toLowerCase(),
+    description: faker.lorem.paragraph(),
+    category: categories[Math.floor(Math.random()*categories.length)],
     img: "",
     visited_count: 0
   }
-];
+  default_hobbies.push(hobby);
+}
+
+// export const default_hobbies = [
+//   {
+//     name: "Menyanyi",
+//     slug: "menyanyi",
+//     description: "Menyanyi merupakan salah satu hobi yang populer di dunia.",
+//     category: "audio-visual",
+//     img: "",
+//     visited_count: 0
+//   },
+//   {
+//     name: "Menari",
+//     slug: "menari",
+//     description: "Menari merupakan salah satu hobi yang populer di dunia.",
+//     category: "audio-visual",
+//     img: "",
+//     visited_count: 0
+//   },
+//   {
+//     name: "Coding",
+//     slug: "coding",
+//     description: "Coding adalah memuat program seperti website.",
+//     category: "teknologi",
+//     img: "",
+//     visited_count: 0
+//   }
+// ];
 
 export const default_suggestions = [
   {
