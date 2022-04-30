08a84ac0554f8fc693f0fd069ebcc56cabce5915
Adhy Wiranto <adhywiranto68@gmail.com>
2022-05-01 04:51:05 +0700

    fix: env for backend domain

--- frontend-visitor/pages/api/category.js
+++ frontend-visitor/pages/api/category.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const url = `${"https://findmyhobby.herokuapp.com" || "http://localhost:8080"}/api/v1/categories`;
+const url = `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/api/v1/categories`;
 
 export const getCategories = async () => await axios.get(`${url}`);
--- frontend-visitor/pages/api/hobby.js
+++ frontend-visitor/pages/api/hobby.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = `${"https://findmyhobby.herokuapp.com" || "http://localhost:8080"}/api/v1/hobbies`;
+const url = `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/api/v1/hobbies`;
 
 export const getHobbies = async () => await axios.get(`${url}`);
 export const getHobbiesByName = async (name = "") => await axios.get(`${url}?name=${name}`);
