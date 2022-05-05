122a4db2ea5027911c4768121e15870eb8aaaff4
Adhy Wiranto <adhywiranto68@gmail.com>
2022-05-05 21:01:47 +0700

    fix: auto connect db every initialization

--- backend/src/database/Connection.ts
+++ backend/src/database/Connection.ts
@@ -6,6 +6,10 @@ class Connection {
   localConnectionURI: string = `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`;
   cloudConnectionURI: string = `mongodb+srv://find-my-hobby-admin:${process.env.DB_PASSWORD}@cluster0.k9fdy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
+  constructor() {
+    this.connect();
+  }
+
   connect() {
     switch(process.env.ENVIRONMENT) {
       case LOCAL_ENV:
@@ -26,6 +30,10 @@ class Connection {
       default:
         return createConnection(this.localConnectionURI);
     }
+  }
+
+  dropDatabase() {
+    return connection.dropDatabase();
   }
 
   closeConnection() {
