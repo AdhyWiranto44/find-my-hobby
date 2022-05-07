e7e65685e43e1a7e148673a12dd09a7c2225ac48
Adhy Wiranto <adhywiranto68@gmail.com>
2022-05-07 13:59:27 +0700

    fix: key category links

--- frontend-visitor/components/navbarHobby.js
+++ frontend-visitor/components/navbarHobby.js
@@ -27,9 +27,9 @@ export default function NavbarHobby() {
       return (
         categories.map((category, idx) => {
           return (
-            <li>
-              <Link key={idx} href={`/hobbies/category/${category.slug}`}>
-                <a class="dropdown-item">{category.name}</a>
+            <li key={idx}>
+              <Link href={`/hobbies/category/${category.slug}`}>
+                <a className="dropdown-item">{category.name}</a>
               </Link>
             </li>
           );
@@ -66,15 +66,15 @@ export default function NavbarHobby() {
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
           <div className="navbar-nav d-flex align-items-center justify-content-between">
-            <div class="dropdown">
-              <button class="bg-transparent border-0 dropdown-toggle nav-link fw-bold" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            <div className="dropdown">
+              <button className="bg-transparent border-0 dropdown-toggle nav-link fw-bold" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-tag me-1" viewBox="0 0 16 16">
                   <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
                   <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
                 </svg>
                 Kategori
               </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                 {renderCategories()}
               </ul>
             </div>
