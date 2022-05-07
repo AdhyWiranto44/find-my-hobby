7c81ee9f0bea2058bbe43e6e2c60e1b616acf124
Adhy Wiranto <adhywiranto68@gmail.com>
2022-05-07 15:10:51 +0700

    fix: endpoints

--- frontend-visitor/pages/api/hobby.js
+++ frontend-visitor/pages/api/hobby.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import domain from '../../constants/domain';
 
 
-export const getHobbies = async () => await axios.get(`${domain}`);
-export const getHobbiesByName = async (name = "") => await axios.get(`${domain}?name=${name}`);
-export const getHobbiesByCategory = async (category = "") => await axios.get(`${domain}/categories/${category}`);
-export const getHobby = async (slug = "") => await axios.get(`${domain}/${slug}`);
+export const getHobbies = async () => await axios.get(`${domain}/hobbies`);
+export const getHobbiesByName = async (name = "") => await axios.get(`${domain}/hobbies?name=${name}`);
+export const getHobbiesByCategory = async (category = "") => await axios.get(`${domain}/hobbies/categories/${category}`);
+export const getHobby = async (slug = "") => await axios.get(`${domain}/hobbies/${slug}`);
