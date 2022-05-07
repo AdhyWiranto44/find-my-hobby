6ae405e2cb4e7045e9e499a66c84815804602d36
Adhy Wiranto <adhywiranto68@gmail.com>
2022-05-07 13:55:05 +0700

    feat: frontend-visitor login to frontend-admin

--- frontend-visitor/components/footer.js
+++ frontend-visitor/components/footer.js
@@ -1,10 +1,12 @@
 export default function Footer() {
+  const domain = process.env.NEXT_PUBLIC_ADMIN_DOMAIN || "http://localhost:3001"
+
   return (
     <footer id="footer" className="bg-warning mt-auto">
       <div className="container">
         <div className="row border-dark">
           <div className="col-md text-center py-3">
-            <small className="mr-2">&#169; {new Date().getFullYear()}. Made with <a className="text-decoration-none text-dark" href="/login">&#10084;</a> By Adhy Wiranto</small>
+            <small className="mr-2">&#169; {new Date().getFullYear()}. Made with <a className="text-decoration-none text-dark" href={`${domain}/login`}>&#10084;</a> By Adhy Wiranto</small>
           </div>
         </div>
       </div>
