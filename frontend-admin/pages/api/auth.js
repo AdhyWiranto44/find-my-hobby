7503cf4a04933bb47905d800bec886b2e52934dd
Adhy Wiranto <adhywiranto68@gmail.com>
2022-05-07 15:12:14 +0700

    fix: endpoint

--- frontend-admin/pages/api/auth.js
+++ frontend-admin/pages/api/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import domain from '../../constants/domain'
 
-const domain = process.env.NEXT_PUBLIC_BACKEND_DOMAIN || "http://localhost:8080/api/v1"
+
 export const login = (form) => axios.post(`${domain}/login`, form)
