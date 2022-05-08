6601d6671b85e705f2a70240e91a7cefd257195a
Adhy Wiranto <adhywiranto68@gmail.com>
2022-05-08 19:24:55 +0700

    feat: back button from login page

--- frontend-admin/pages/login.js
+++ frontend-admin/pages/login.js
@@ -4,6 +4,7 @@ import Title from '../components/title'
 import { login } from './api/auth'
 import Cookies from 'js-cookie'
 import { tokenCookie } from '../constants/cookies'
+import Link from 'next/link'
 
 
 export default function Login() {
@@ -43,7 +44,16 @@ export default function Login() {
                 <label htmlFor="password" className="form-label small mb-1">password</label>
                 <input type="password" className="form-control p-3" id="password" onChange={(e) => setForm({...form, "password": e.target.value})} name="password" required />
               </div>
-              <button type="submit" className="btn btn-salmon w-100 p-3 mt-3 fw-bold text-uppercase">login</button>
+              <div className="row gx-3">
+                <div className="col-sm-3">
+                  <Link href={`${process.env.NEXT_PUBLIC_VISITOR_DOMAIN || "http://localhost:3000"}`}>
+                    <a className="btn btn-outline-secondary w-100 p-3 mt-3"><i class="bi bi-chevron-left"></i></a>
+                  </Link>
+                </div>
+                <div className="col-sm-9">
+                  <button type="submit" className="btn btn-salmon w-100 p-3 mt-3 fw-bold text-uppercase">login</button>
+                </div>
+              </div>
             </form>
           </div>
           <div className="col-lg-6 d-none d-lg-block p-0 overflow-hidden">
