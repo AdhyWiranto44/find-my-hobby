86ec67cc99e28e7bfb916735d01d6def574048c9
Adhy Wiranto <adhywiranto68@gmail.com>
2022-05-11 06:21:29 +0700

    feat: get all data

--- frontend-admin/pages/api/category.js
+++ frontend-admin/pages/api/category.js
@@ -3,8 +3,8 @@ import domain from '../../constants/domain'
 import headers from '../../constants/headers'
 
 
-export const getCategories = () => axios.get(`${domain}/categories`)
-export const getCategoriesByName = async (name = "") => await axios.get(`${domain}/categories?name=${name}`)
+export const getCategories = () => axios.get(`${domain}/categories?limit=0&skip=0`)
+export const getCategoriesByName = async (name = "") => await axios.get(`${domain}/categories?name=${name}&limit=0&skip=0`)
 export const getCategory = async (slug = "") => await axios.get(`${domain}/categories/${slug}`)
 export const createCategory = async (form = {}) => await axios.post(`${domain}/categories`, form, headers)
 export const updateCategory = async (slug = "", form = {}) => await axios.patch(`${domain}/categories/${slug}`, form, headers)
