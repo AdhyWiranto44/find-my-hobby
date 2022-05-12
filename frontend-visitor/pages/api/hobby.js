22ba1a5f4e3aad758d2e062571581020f0812b76
adhywiranto44 <adhywiranto68@gmail.com>
2022-05-13 02:34:08 +0700

    fix: get hobbies by category

--- frontend-visitor/pages/api/hobby.js
+++ frontend-visitor/pages/api/hobby.js
@@ -4,5 +4,5 @@ import domain from '../../constants/domain';
 
 export const getHobbies = async () => await axios.get(`${domain}/hobbies?limit=0&skip=0`);
 export const getHobbiesByName = async (name = "") => await axios.get(`${domain}/hobbies?name=${name}&limit=0&skip=0`);
-export const getHobbiesByCategory = async (category = "") => await axios.get(`${domain}/hobbies/categories/${category}`);
+export const getHobbiesByCategory = async (category = "") => await axios.get(`${domain}/hobbies/categories/${category}?limit=0&skip=0`);
 export const getHobby = async (slug = "") => await axios.get(`${domain}/hobbies/${slug}`);
