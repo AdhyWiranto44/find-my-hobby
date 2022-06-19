a63fa9e12fe34f08406ab31c525fd1ef25fd62c8
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-06-19 16:37:49 +0700

    feat: connection using postgres

---
+++ backend/src/database/ConnectionPostgres.ts
@@ -0,0 +1,26 @@
+import { connect, connection, createConnection } from "mongoose";
+import { Sequelize } from "sequelize";
+import { LOCAL_ENV, PRODUCTION_ENV } from "../helpers/constants";
+
+
+class ConnectionPostgres {
+
+  static connection: any = null;
+
+  static connect() {
+    if (ConnectionPostgres.connection === null) {
+      console.log("Creating new connection...");
+      ConnectionPostgres.connection = new Sequelize('postgres://postgres:12345@localhost:5432/findmyhobby_development');
+      console.log("New connetion created.");
+    }
+
+    return ConnectionPostgres.connection;
+  }
+
+  closeConnection() {
+    // Do something.
+  }
+}
+
+
+export default ConnectionPostgres;
