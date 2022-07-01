eeae4f4ff6a7d04ff1509a7cb1e33f39410b6906
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-07-01 13:10:35 +0700

    feat: image in hobby item

--- frontend-visitor/components/hobbyItem.js
+++ frontend-visitor/components/hobbyItem.js
@@ -3,12 +3,19 @@ import Image from 'next/image'
 
 
 export default function HobbyItem(props) {
+  const renderImage = (image) => {
+    if (image && image !== null && image !== "") {
+      return <Image src={`${process.env.NEXT_PUBLIC_IMG_DIR}/${image}`} className="card-img w-100" width={100} height={300} alt="Gambar Hobi" />
+    }
+    return <Image src={`/img/hobi.webp`} className="card-img w-100" width={100} height={300} alt="Gambar Hobi" />
+  }
+
   return (
     <div id="hobbyItem" className="col-6 col-md-4 col-lg-3">
       <Link href={`/hobby/${props.hobby.slug}`}>
         <a>
           <div className="card bg-dark text-white border-0 shadow overflow-hidden mb-4" style={{ borderRadius: "15px" }}>
-            <Image src="/img/hobi.webp" className="card-img w-100" width={100} height={300} alt="Gambar Hobi" />
+            {renderImage(props.hobby.img)}
             <div className="card-img-overlay d-flex">
               <div className="mt-auto">
                 <h6 className="card-title fw-bold">{props.hobby.name}</h6>
